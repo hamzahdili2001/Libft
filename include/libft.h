278b08934b2405c0f3af442113f2838a451ccff0
hamzahdili2001 <hdilihamza955@gmail.com>
2026-08-20 21:15:01 +0100

    header file

---
+++ include/libft.h
@@ -0,0 +1,10 @@
+#ifndef LIBFT_H
+# define LIBFT_H
+
+typedef unsigned int	t_uint;
+
+t_uint					ft_strlen(char *str);
+int						ft_putchar(char c);
+void					ft_putstr(char *str);
+char					*ft_strcpy(char *dest, const char *src);
+#endif
